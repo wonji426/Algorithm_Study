26c8e527785d9bf68668c9b6b974f51669673883
yeseul <144925533+Yuyeseul@users.noreply.github.com>
2026-06-05 22:33:51 +0900

    [PRO] 12918 / 유예슬 / Lv.1

---
+++ Programmers/1Week/12918_문자열 다루기 기본/yeseul.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = true;
+        answer = (s.length()==4||s.length()==6)&&s.matches("^[0-9]+$");
+        return answer;
+    }
+}
