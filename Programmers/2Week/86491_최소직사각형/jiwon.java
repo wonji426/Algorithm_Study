5573ec3be367b65c2780b2ae7d821cd40fa8e03d
jiwonKang <kjw042626@gmail.com>
2026-06-08 01:59:56 +0900

    [PRO] 86491 / 강지원 / Lv.1
    Implement solution to find minimum rectangle area from sizes.

---
+++ Programmers/2Week/86491_최소직사각형/jiwon.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        int maxV=0;
+        int maxH=0;
+        for(int i=0;i<sizes.length;i++){
+            int v=Math.max(sizes[i][0],sizes[i][1]);
+            int h=Math.min(sizes[i][0],sizes[i][1]);
+            maxV=Math.max(maxV,v);
+            maxH=Math.max(maxH,h);
+        }
+        return answer=maxV*maxH;
+    }
+}
