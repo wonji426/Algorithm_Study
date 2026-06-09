86c96b413619b6c2f5d48ac5d1177e0ef3b863ed
jiwonKang <kjw042626@gmail.com>
2026-06-10 00:26:26 +0900

    [PRO] 138477 / 강지원 / Lv.1

---
+++ Programmers/2Week/138477_명예의 전당 (1)/jiwon.java
@@ -0,0 +1,19 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for(int i=0; i<score.length; i++) {
+            pq.offer(score[i]);
+            if(pq.size() > k) {
+                pq.poll();
+            }
+            answer[i] = pq.peek();
+        }
+        
+        return answer;
+    }
+}
