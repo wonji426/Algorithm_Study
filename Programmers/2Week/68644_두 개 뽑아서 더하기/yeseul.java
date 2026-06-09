e1491e6e5ce90b1bf256849aedb64bfca3002ac1
yeseul <144925533+Yuyeseul@users.noreply.github.com>
2026-06-09 21:50:52 +0900

    [PRO] 68644 / 유예슬 / Lv.1

---
+++ Programmers/2Week/68644_두 개 뽑아서 더하기/yeseul.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < numbers.length - 1; i++){
+            for(int j = i + 1; j < numbers.length; j++){
+                int sum = numbers[i] + numbers[j];
+                if(!list.contains(sum)){
+                    list.add(sum);
+                }
+            }
+        }
+        Collections.sort(list);
+        int[] answer = new int[list.size()];
+        for(int i = 0; i < list.size(); i++){
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
