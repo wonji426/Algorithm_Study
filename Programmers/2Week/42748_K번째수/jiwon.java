e6c4c5181075949ad5c60bb2074eb58c3a9ca71a
jiwonKang <kjw042626@gmail.com>
2026-06-10 00:27:57 +0900

    [PRO] 42748 / 강지원 / Lv.1

---
+++ Programmers/2Week/42748_K번째수/jiwon.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for(int i=0; i< commands.length; i++) {
+            int start = commands[i][0] - 1;
+            int end = commands[i][1];
+            int k = commands[i][2];
+            
+            int[] temp = Arrays.copyOfRange(array, start, end);
+            Arrays.sort(temp);
+            answer[i] = temp[k - 1];
+        }
+        
+        
+        return answer;
+    }
+}
