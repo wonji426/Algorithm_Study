dcc7394e25127e90153a089470d2e61fc6a2fe02
yeseul <144925533+Yuyeseul@users.noreply.github.com>
2026-06-28 23:44:21 +0900

    [PRO] 77484 / 유예슬 / Lv.1
    Implement solution for lotto ranking based on winning numbers.

---
+++ Programmers/4Week/77484_로또의 최고 순위와 최저 순위/yeseul.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int[] answer = new int[2];
+        int count = 0;
+        int zero = 0;
+        for(int i = 0; i < lottos.length; i++){
+            if(lottos[i] == 0){
+                zero++;
+            }else{
+                for(int j = 0; j < win_nums.length; j++){
+                    if(lottos[i] == win_nums[j]){
+                        count++;
+                    }
+                }
+            }
+        }
+        System.out.println(count + " " +zero);
+        answer[0] = (7 - (count + zero)) >= 6 ? 6 : 7 - (count + zero);
+        answer[1] = (7 - count) >= 6 ? 6 : 7-count;
+        
+    
+        return answer;
+    }
+}
